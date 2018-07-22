edf903403bf30416081edb64bca6c118cc5d25a3
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-22 23:30:02 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,4 +27,3 @@ epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-end
